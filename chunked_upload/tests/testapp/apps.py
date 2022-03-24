84c710aee90b1df1f54a80e4e8696f1a6c6676f8
Jaryd Rester <github@jaryd.xyz>
2022-03-23 20:38:43 -0500

    setting up tests

---
+++ chunked_upload/tests/testapp/apps.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Setups an app for testing the pluggable app."""
+
+__author__ = "Jaryd Rester"
+__copyright__ = "2022-03-23"
+
+# stdlib
+
+# django
+from django.apps import AppConfig
+
+# local django
+
+# thirdparty
+
+
+class TestAppConfig(AppConfig):
+    name = "chunked_upload.tests.testapp"
+    verbose_name = "TestApp"
